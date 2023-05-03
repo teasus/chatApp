3577d2f58428de39893ecc59ae74891f2913b861
A-RA <ahmedrashid619@.com>
2023-05-04 04:52:10 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDJ5146lf6ItHYZzLOJqU9vXT2WqlxoYtU",
+  authDomain: "chatapp-ara-f16f9.firebaseapp.com",
+  projectId: "chatapp-ara-f16f9",
+  storageBucket: "chatapp-ara-f16f9.appspot.com",
+  messagingSenderId: "646071232937",
+  appId: "1:646071232937:web:e7292156d85c307b667cb8",
+};
+
+export const app = initializeApp(firebaseConfig);
